051cf5c3b0a80255bb3ef243a4d5af87878d9fbc
fengzhbo <563128453@qq.com>
2015-02-15 00:03:42 +0800

    create

---
+++ ToolsTest.NUnitTest/TestCaseFirst.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ToolsTest.FrontEnd;
+
+namespace ToolsTest.NUnitTest
+{
+    [TestFixture]
+    public class TestCaseFirst
+    {
+        [TestCase]
+        public void TestForTest()
+        {
+            var forTestInstance = new Moq.Mock<IForTest>();
+
+            forTestInstance.Setup(test => test.HandleString("test")).Returns("TEST");
+
+            Assert.AreEqual("TEST",forTestInstance.Object.HandleString("test"));
+
+        }
+    }
+}
