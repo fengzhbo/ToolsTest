6bb6440065f885b7318d2f97615cc13abaa83c32
fengzhbo <563128453@qq.com>
2015-02-15 16:24:06 +0800

    test autofac

---
+++ ToolsTest.FrontEnd/AutoFacTest/ITestInterface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsTest.FrontEnd.AutoFacTest
+{
+    public interface ITestPrice
+    {
+        Int32 WirteData(Int32 price);
+
+        List<Int32> GetPrice();
+
+    }
+
+    public interface ITestDealer
+    {
+
+        Int32 AddDealer(Int32 dealerID);
+
+        List<Int32> GetDealer();
+
+    }
+}
---
+++ ToolsTest.FrontEnd/AutoFacTest/TestService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsTest.FrontEnd.AutoFacTest
+{
+    public class TestService
+    {
+        ITestPrice _priceDal;
+        ITestDealer _dealerDal;
+
+        public TestService(ITestDealer dealerDal, ITestPrice priceDal)
+        {
+            _priceDal = priceDal;
+            _dealerDal = dealerDal;
+
+        }
+
+        public Int32 RegisterDealer(Int32 dealerId)
+        {
+            return _dealerDal.AddDealer(dealerId);
+        }
+
+        public List<Int32> GetDealers()
+        {
+            return _dealerDal.GetDealer();
+        }
+
+        public Int32 PublicPrice(Int32 price)
+        {
+            return _priceDal.WirteData(price);
+        }
+
+    }
+}
