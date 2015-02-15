a1126a5fe17726ffbe9d830c6601ffd3948018f9
fengzhbo <563128453@qq.com>
2015-02-15 16:25:41 +0800

    遗漏的文件

---
+++ ToolsTest.FrontEnd/AutoFacTest/ImplementSecond.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsTest.FrontEnd.AutoFacTest
+{
+    public class ImplementSecondPrice : ITestPrice
+    {
+        private HashSet<Int32> priceSet = new HashSet<int>();
+
+        public int WirteData(int price)
+        {
+            if (priceSet.Add(price))
+            {
+                return price;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public List<int> GetPrice()
+        {
+            return priceSet.ToList<Int32>();
+        }
+    }
+
+    public class ImplementSecondDealer : ITestDealer
+    {
+        private HashSet<Int32> dealerSet = new HashSet<int>();
+
+        public int AddDealer(int dealerID)
+        {
+            if (dealerSet.Add(dealerID))
+            {
+                return dealerID;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public List<int> GetDealer()
+        {
+            return dealerSet.ToList<Int32>();
+        }
+    }
+}
